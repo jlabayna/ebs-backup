3645b23e6a5b5c5948f5b6851f67904b74b6e56c
jlabayna <james@jameslabayna.com>
2022-04-22 22:04:02 -0400

    Add check for jq installation
    jq is used to parse aws output (and maybe more)

--- ebs-backup.sh
+++ ebs-backup.sh
@@ -2,10 +2,18 @@
 
 alias error='echo 1>&2'
 
+# Check if necessary commands exist
+
 # Check if rsync is installed as a safety measure in case of incorrect
 # installation.
 # TODO: Remove me if non-exitance of rsync won't cause issue.
 if ! command -v rsync >/dev/null; then
   error "Please install rsync(1)"
+  exit 1
+fi
+
+# jq is used to parse aws output
+if ! command -v jq >/dev/null; then
+  error "Please install jq(1)"
   exit 1
 fi
